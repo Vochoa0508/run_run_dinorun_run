5161940b26a01f09dee528e2d47e97d33a03499c
Vochoa <victoriaolguin2008@gmail.com>
2024-05-27 11:00:01 -0600

    nuevo avance

--- Main.gd
+++ Main.gd
@@ -8,6 +8,9 @@ func _ready():
 	screen_size = get_window().size
 	PlayerInitialPosition = $player.position
 
+func new_game():
+	$Floor.position.x = 0
+	$player.position = PlayerInitialPosition
 
 func _process(delta):
 	$Floor.position.x = $player.position.x - 150
